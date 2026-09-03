9d375c9a943724f467de972c91c0e7a9b3217b83
Vadym <minerking112256@gmail.com>
2026-09-03 10:56:08 +0200

    Create HttpResponse.cs

---
+++ src/HttpServer/HttpResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace HttpServer;
+
+public static class HttpResponse
+{
+    public static byte[] CreateResponse(string message, string contentType = "text/plain")
+    {
+        byte[] bodyBytes = Encoding.UTF8.GetBytes(message);
+        string headers =    $"HTTP/1.1 200 OK\r\n" + 
+                            $"Content-Type: {contentType}\r\n" + 
+                            $"Content-Length: {bodyBytes.Length}\r\n\r\n";
+        byte[] headerBytes = Encoding.UTF8.GetBytes(headers);
+        return [.. headerBytes, .. bodyBytes];
+    }
+}

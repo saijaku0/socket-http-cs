fe1edfef8e2490b6ea795b0153cb0b3003f09dde
Vadym <minerking112256@gmail.com>
2026-09-06 15:11:05 +0200

    Update HttpResponse.cs

--- src/HttpServer/HttpResponse.cs
+++ src/HttpServer/HttpResponse.cs
@@ -2,15 +2,33 @@
 
 namespace HttpServer;
 
-public static class HttpResponse
+public record HttpResponse(
+    int StatusCode,
+    string Body,
+    string ContentType = "text/plain")
 {
-    public static byte[] CreateResponse(string message, string contentType = "text/plain")
+    public byte[] ToBytes()
     {
-        byte[] bodyBytes = Encoding.UTF8.GetBytes(message);
-        string headers =    $"HTTP/1.1 200 OK\r\n" + 
-                            $"Content-Type: {contentType}\r\n" + 
-                            $"Content-Length: {bodyBytes.Length}\r\n\r\n";
+        byte[] bodyBytes = Encoding.UTF8.GetBytes(Body);
+
+        string statusText = StatusCode switch
+        {
+            200 => "OK",
+            201 => "Created",
+            400 => "Bad Request",
+            404 => "Not Found",
+            500 => "Internal Server Error",
+            _ => "Unknown"
+        };
+
+        string headers =
+            $"HTTP/1.1 {StatusCode} {statusText}\r\n" +
+            $"Content-Type: {ContentType}\r\n" +
+            $"Content-Length: {bodyBytes.Length}\r\n" +
+            "\r\n";
+
         byte[] headerBytes = Encoding.UTF8.GetBytes(headers);
+
         return [.. headerBytes, .. bodyBytes];
     }
 }
